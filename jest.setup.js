0512ced8d26b41765dab27fd5e337f8a8ad898a8
Flaviano Redressa <flaviano@operative.com.br>
2025-07-19 17:34:42 -0300

    feature adicionado jest dentro do projeto

---
+++ jest.setup.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+
+// Mock next/router
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    pathname: '/',
+    query: {},
+    asPath: '/',
+  }),
+}));
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    pathname: '/',
+    query: {},
+    asPath: '/',
+  }),
+  usePathname: () => '/',
+}));
